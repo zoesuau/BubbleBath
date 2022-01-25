c25aea85deced269fa6d833a82fd625ac135a1cb
zoesu <zoesuau@gmail.com>
2022-01-25 20:53:33 +0800

    no message

--- public/js/index.js
+++ public/js/index.js
@@ -46,3 +46,14 @@ $(".mobile_header").on("click",function(){
      
           }
         })
+
+        wow = new WOW(
+          {
+          boxClass:     'wow',      // default
+          animateClass: 'animated', // default
+          offset:    0,          // default
+          mobile:       true,       // default
+          live:         true        // default
+        }
+        )
+        wow.init();
